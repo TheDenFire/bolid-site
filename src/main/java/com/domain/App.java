96de941c034371634b75a7b67f4c8aaf168fd5ea
Bogdan <82058062+TheDenFire@users.noreply.github.com>
2025-05-28 19:45:12 +0300

    fix: manifest domain class undefied

--- src/main/java/com/domain/App.java
+++ src/main/java/com/domain/App.java
@@ -1,15 +1,11 @@
 package com.domain;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class App {
     public static void main(String[] args) {
         try {
-            Dotenv dotenv = Dotenv.configure().load();
-            String telegramToken = System.getenv("TG_TOKEN");
-            String dbUrl = System.getenv("TG_TOKEN");
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new HistoryBot());
         } catch (Exception e) {
--- src/main/java/com/domain/Config.java
+++ src/main/java/com/domain/Config.java
@@ -1,10 +1,6 @@
 package com.domain;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
-
     public static String getBotUsername() {
         return System.getenv("TG_TOKEN");
     }

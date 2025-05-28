3ce8d23b8b6d882f6899055b9654a20511d20491
Bogdan <82058062+TheDenFire@users.noreply.github.com>
2025-05-28 19:59:45 +0300

    fix: manifest domain class undefied

--- src/main/java/com/domain/App.java
+++ src/main/java/com/domain/App.java
@@ -1,10 +1,13 @@
 package com.domain;
 
+import jakarta.persistence.EntityManager;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class App {
     public static void main(String[] args) {
+        EntityManager em = EmUtill.getEntityManager();
+        em.close();
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new HistoryBot());

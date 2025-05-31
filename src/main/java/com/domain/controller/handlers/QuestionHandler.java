92d8fe9878d402bdaf842bd2f33811d110b10f8f
Bogdan <82058062+TheDenFire@users.noreply.github.com>
2025-05-31 21:42:09 +0300

    test: adding log for test

--- src/main/java/com/domain/controller/handlers/QuestionHandler.java
+++ src/main/java/com/domain/controller/handlers/QuestionHandler.java
@@ -5,6 +5,8 @@ import com.domain.models.Question;
 import com.domain.models.User;
 import com.domain.service.QuestionService;
 import com.domain.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
@@ -19,6 +21,7 @@ public class QuestionHandler extends BaseHandler{
 
     private static final QuestionService questionService = new QuestionService();
     private static final UserService userService = new UserService();
+    private static final Logger log = LoggerFactory.getLogger(QuestionService.class);
 
     private final Map<Long, LinkedList<Question>> session = new HashMap<>();
 
@@ -75,6 +78,7 @@ public class QuestionHandler extends BaseHandler{
 
                     User user = userService.findById(userId);
                     if(user.getScore()!=null){
+                        log.info("user score is " + user.getScore());
                         user.setScore(user.getScore() + 1);
                     }
 
